c870698c4b2cc9a5ac32f8837f8f747bc5a89548
Aret Mimaryan <amimaryan@gmail.com>
2020-02-04 23:20:49 -0500

    5.3: Edit Quesiton Component in Angular

--- src/app/question.component.ts
+++ src/app/question.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core'
 import { ApiService } from './api.service'
 import { THIS_EXPR } from '@angular/compiler/src/output/output_ast'
+import { throwToolbarMixedModesError } from '@angular/material';
 
 @Component({
     selector: 'question',
@@ -12,6 +13,10 @@ export class QuestionComponent {
     question = {};
     
     constructor(private api: ApiService){}
+
+    ngOnInit() {
+        this.api.questionSelected.subscribe(question => this.question = question)
+    }
 
     post(question) {
         this.api.postQuestion(question)
